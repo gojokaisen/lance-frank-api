42435e1a3f5eaa36f922d92b57b94208681fa0e1
lance-ui <149887891+lance-ui@users.noreply.github.com>
2025-01-01 21:24:09 +0200

    Update anime-random.js

--- api/anime-random.js
+++ api/anime-random.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 exports.config = {
     name: 'anime-random',
-    author: 'AceGerome',
+    author: 'lanxfrank',
     description: 'Fetch a random anime character from the list.',
     method: 'get',
     category: 'anime',
